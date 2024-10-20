2bcd564db70e65041f5275eae18107ec6bff5b29
GuttedOnions <151013341+GuttedOnions@users.noreply.github.com>
2024-10-20 17:39:54 +0200

    main menu done!

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,4 +9,10 @@ public class MainMenu : MonoBehaviour {
     {
         SceneManager.LoadScene("SampleScene");
     }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
 }
